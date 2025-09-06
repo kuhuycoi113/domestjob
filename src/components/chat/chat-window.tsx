85a2f23c1a618e9437ed8c22a19ce65444f5bc0b
ベトグェン <beto.hanoi@gmail.com>
2025-09-06 08:49:53 +0000

    thay giá trị của CHATNAME1 thành đường dẫn "HelloJob" và luôn cố định gi

--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -114,7 +114,7 @@ export function ChatWindow({ conversation }: ChatWindowProps) {
           <div>
              <div className="flex items-center gap-2">
                  {/* CHATNAME1 */}
-                 <p className="text-sm font-bold font-headline leading-tight">{displayContact.name}</p>
+                 <p className="text-sm font-bold font-headline leading-tight">HelloJob</p>
                  <div className="w-2 h-2 rounded-full bg-green-400"></div>
              </div>
             <p className="text-xs text-primary-foreground/80 font-semibold">Đang hoạt động</p>

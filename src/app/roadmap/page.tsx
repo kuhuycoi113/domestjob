38d30e49b067b96adeca982baae83a9cb2f8230f
ベトグェン <beto.hanoi@gmail.com>
2025-09-08 23:20:31 +0000

    sửa thành "Đây là hình thức phổ biến nhất với thời hạn 3-5 năm, dành cho

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -15,7 +15,7 @@ const programTypes = [
   {
     icon: HardHat,
     title: 'Thực tập sinh kỹ năng',
-    description: 'Đây là hình thức phổ biến nhất với thời hạn 3-5 năm, không yêu cầu bằng cấp, không cần thi tuyển tay nghề và có thể nâng cấp lên visa Kỹ năng đặc định sau khi hoàn thành.',
+    description: 'Đây là hình thức phổ biến nhất với thời hạn 3-5 năm, dành cho lao động phổ thông không yêu cầu bằng cấp, không cần thi tuyển tay nghề và có thể nâng cấp lên visa Kỹ năng đặc định sau khi hoàn thành.',
     color: 'orange'
   },
   {

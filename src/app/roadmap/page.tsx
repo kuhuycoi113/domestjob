879eacac893e45117759029f0fac07557adcddaa
ベトグェン <beto.hanoi@gmail.com>
2025-09-08 23:14:50 +0000

    ghi chú thêm có thể xin visa vĩnh trú, định cư và bảo lãnh gia đình sang

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -27,7 +27,7 @@ const programTypes = [
   {
     icon: Briefcase,
     title: 'Kỹ sư, tri thức',
-    description: 'Dành cho người có trình độ chuyên môn cao (Cao đẳng, Đại học), mở ra cơ hội phát triển sự nghiệp lâu dài và bảo lãnh người thân.',
+    description: 'Dành cho người có trình độ chuyên môn cao (Cao đẳng, Đại học), mở ra cơ hội xin visa vĩnh trú, định cư và bảo lãnh gia đình sang Nhật sinh sống.',
     color: 'green'
   }
 ];
